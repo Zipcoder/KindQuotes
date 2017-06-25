b36e584b2dd19982c299a81b3c794df9cce7d08c
Mark A Brown <markabrown87@gmail.com>
2017-06-25 16:56:36 -0400

    CRUD Operations in Server

--- server/src/main/java/io/zipcoder/kindquotes/controller/QuoteController.java
+++ server/src/main/java/io/zipcoder/kindquotes/controller/QuoteController.java
@@ -1,25 +1,55 @@
 package io.zipcoder.kindquotes.controller;
 
 import io.zipcoder.kindquotes.model.Quote;
+import io.zipcoder.kindquotes.repository.QuoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import java.net.URI;
 
 @RequestMapping("/quotes")
 @RestController
 @CrossOrigin("http://localhost:8100")
 public class QuoteController {
 
+    @Autowired
+    QuoteRepository quoteRepository;
+
     @RequestMapping("/")
-    public ResponseEntity<ArrayList<Quote>> getAllQuotes(){
-        Quote quote = new Quote("Wu Tang Clan!!!");
-        ArrayList<Quote> quotes = new ArrayList<>();
-        quotes.add(quote);
-        return new ResponseEntity<>(quotes, HttpStatus.OK);
+    public ResponseEntity<Iterable<Quote>> getAllQuotes(){
+        return new ResponseEntity<>(quoteRepository.findAll(), HttpStatus.OK);
     }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ResponseEntity<Quote> addQuote(@RequestBody Quote quote){
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newPollUri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(quote.getID())
+                .toUri();
+        responseHeaders.setLocation(newPollUri);
+        return new ResponseEntity<Quote>(quoteRepository.save(quote), HttpStatus.OK);
+    }
+
+    @RequestMapping("/{id}/")
+    public ResponseEntity<Quote> getQuote(@PathVariable long id) {
+        return new ResponseEntity<Quote>(quoteRepository.findOne(id), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}/", method = RequestMethod.PUT)
+    public ResponseEntity<Quote> updateQuote(@PathVariable long id, @RequestBody Quote quote) {
+        return new ResponseEntity<Quote>(quoteRepository.save(quote), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}/", method = RequestMethod.DELETE)
+    public ResponseEntity<Quote> deleteQuote(@PathVariable long id) {
+        quoteRepository.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 
 }
--- server/src/main/java/io/zipcoder/kindquotes/model/Quote.java
+++ server/src/main/java/io/zipcoder/kindquotes/model/Quote.java
@@ -1,9 +1,22 @@
 package io.zipcoder.kindquotes.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
 public class Quote {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "QUOTE_ID")
+    private long id;
+
+    @Column(name = "QUOTE_MESSAGE")
     private String message;
+
+    public Quote () { this.message = "default"; }
 
     public Quote(String msg){
         this.message = msg;
@@ -16,4 +29,9 @@ public class Quote {
     public void setMessage(String message) {
         this.message = message;
     }
+
+    public long getID() { return id; }
+
+    public void setID(long id) {this.id = id;}
+
 }
